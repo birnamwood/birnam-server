f39e46b906855a39814592103a44a323e9943d3d
birnamwood <birnamwood19@gmail.com>
2021-06-13 17:07:23 +0900

    port設定変更

--- init/router/router.go
+++ init/router/router.go
@@ -1,22 +1,20 @@
 package router
 
 import (
-	"os"
-
 	"github.com/birnamwood/birnam-server/pkg/interface/handler"
 	"github.com/labstack/echo"
 )
 
 func Init(port string) {
 	e := echo.New()
-	e.HideBanner = false
-	e.HidePort = true
+	e.HideBanner = true
+	e.HidePort = false
 	// ルーティング設定
 	e.GET("/test", handler.GetTest)
 	e1 := e.Group("/api")
 	{
 		e1.POST("/test", handler.GetTest2)
 	}
-	e.Start(":" + os.Getenv("PORT"))
+	e.Start(":" + port)
 
 }
